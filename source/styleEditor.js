7dfb293a1938fbc4ef95191d0d8712fb54d79a87
nico24687 <theonlynico@hotmail.com>
2018-03-09 19:02:39 -0700

    able to change css of elements

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-  // your code goes here.
+  $(".style-it").on("click", function(){
+    event.preventDefault();
+
+    let selector = $(style_editor[name="selector"]).val();
+    let property = $(style_editor[name="property"]).val();
+    let value = $(style_editor[name="value"]).val()
+
+    $(selector).css(property, value)
+  })
 });
